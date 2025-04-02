e6e9d0492279575e09376d08af92a9bfefee09e5
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-04-03 00:54:10 +0530

    Cleanup research page

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -11,18 +11,7 @@ export default function Research() {
     <section>
       <h1 className="mb-8 text-2xl font-medium">Research</h1>
       <div className="prose prose-neutral dark:prose-invert">
-        <p>
-          My primary research focus is to enhancing the training, fine-tuning,
-          and inference capabilities of pre-trained large language models, while
-          also releasing the datasets used in their training.
-        </p>
-        <p>
-          You can explore my work on{" "}
-          <a href="https://huggingface.co/imsirius" target="_blank">
-            Hugging Face
-          </a>
-          .
-        </p>
+        <p>Still working on them, gonna take a bit ;)</p>
       </div>
     </section>
   );
