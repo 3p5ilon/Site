f27896206c93ee9e56565139dc2b4e8aeaea1a3f
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-09-14 09:36:48 +0530

    Update open graph

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -5,30 +5,32 @@ export function GET(request: Request) {
   const title = url.searchParams.get("title") || "Ɛpsilon";
 
   return new ImageResponse(
-    <div
-      style={{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#fff",
-        fontWeight: 600,
-        position: "relative",
-      }}
-    >
+    (
       <div
         style={{
-          position: "absolute",
-          top: 40,
-          left: 40,
-          fontSize: 30,
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#fff",
+          fontWeight: 600,
+          position: "relative",
         }}
       >
-        Ɛpsilon
+        <div
+          style={{
+            position: "absolute",
+            top: 60,
+            left: 60,
+            fontSize: 40,
+          }}
+        >
+          Ɛpsilon
+        </div>
+        <div style={{ fontSize: 80, textAlign: "center" }}>{title}</div>
       </div>
-      <div style={{ fontSize: 60, textAlign: "center" }}>{title}</div>
-    </div>,
+    ),
     {
       width: 1200,
       height: 630,
