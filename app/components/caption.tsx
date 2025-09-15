c232739475288df9a3e0dacff82603e282272380
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-09-16 04:15:13 +0530

    update caption

--- app/components/caption.tsx
+++ app/components/caption.tsx
@@ -1,16 +1,3 @@
-// import Balancer from "react-wrap-balancer";
-// import type { ReactNode } from "react";
-
-// export function Caption({ children }: { children: ReactNode }) {
-//   return (
-//     <span className=" block w-full text-xs my-4 font-mono text-neutral-600 dark:text-neutral-400 text-center leading-none">
-//       <Balancer>
-//         <span className="[&>a]:post-link">{children}</span>
-//       </Balancer>
-//     </span>
-//   );
-// }
-
 import Balancer from "react-wrap-balancer";
 import type { ReactNode } from "react";
 
