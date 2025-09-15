9d9fcb64e5938c8704698aae515c0f26fc7439f0
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-09-16 05:26:30 +0530

    update goodreads embed

--- app/components/goodreads.tsx
+++ app/components/goodreads.tsx
@@ -65,8 +65,8 @@ export default function NowReading() {
                   loading="eager"
                 />
               </div>
-              <div className="flex-1 min-w-0 pr-10 flex flex-col justify-start self-start mt-0.5 sm:mt-1.5">
-                <div className="text-[14px] sm:text-base font-medium sm:font-semibold leading-4.5 sm:leading-5.5 break-words hyphens-auto">
+              <div className="flex-1 min-w-0 pr-10 flex flex-col justify-start self-start mt-1 sm:mt-1.5">
+                <div className="text-[14px] sm:text-base font-semibold leading-4.5 sm:leading-5.5 break-words hyphens-auto">
                   {book.title}
                 </div>
                 {derivedAuthor && (
