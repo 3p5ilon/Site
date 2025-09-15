59d16933163fb8c1f09a3ef00867104dfe1c6071
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-09-16 03:20:09 +0530

    update caption component

--- app/components/caption.tsx
+++ app/components/caption.tsx
@@ -1,9 +1,22 @@
+// import Balancer from "react-wrap-balancer";
+// import type { ReactNode } from "react";
+
+// export function Caption({ children }: { children: ReactNode }) {
+//   return (
+//     <span className=" block w-full text-xs my-4 font-mono text-neutral-600 dark:text-neutral-400 text-center leading-none">
+//       <Balancer>
+//         <span className="[&>a]:post-link">{children}</span>
+//       </Balancer>
+//     </span>
+//   );
+// }
+
 import Balancer from "react-wrap-balancer";
 import type { ReactNode } from "react";
 
-export function CaptionComponent({ children }: { children: ReactNode }) {
+export function Caption({ children }: { children: ReactNode }) {
   return (
-    <span className="block w-full text-xs my-2 font-mono text-neutral-600 dark:text-[#A1A1A1] text-center leading-normal">
+    <span className="not-prose block w-full text-xs my-4 font-mono text-neutral-600 dark:text-neutral-400 text-center leading-normal">
       <Balancer>
         <span className="[&>a]:post-link">{children}</span>
       </Balancer>
