84323591df2c4089d56cd36359ff69e81e7764af
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2025-10-11 17:02:23 +0530

    update caption embed

--- app/components/caption.tsx
+++ app/components/caption.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 
 export function Caption({ children }: { children: ReactNode }) {
   return (
-    <span className="not-prose block w-full text-xs my-4 font-mono text-neutral-600 dark:text-neutral-400 text-center leading-normal">
+    <span className="not-prose caption block w-full text-xs my-3 font-mono text-neutral-600 dark:text-neutral-400 text-center leading-normal">
       <Balancer>
         <span className="[&>a]:post-link">{children}</span>
       </Balancer>
